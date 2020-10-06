0b63dabfc345f176c7690379df5663bac4d0f273
lwwtl <598449595@qq.com>
2020-10-06 09:37:18 +0800

    base

---
+++ src/main/java/com/pet/demo/PetAdoptionApplication.java
@@ -0,0 +1,13 @@
+package com.pet.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAdoptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetAdoptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pet/demo/PetAdoptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAdoptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
