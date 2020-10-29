df6db797b5e06b10293dd3f20719023cb75ba4de
lwwtl <598449595@qq.com>
2020-10-29 15:52:39 +0800

    viewcontroller

--- src/main/java/com/pet/demo/config/MyWebMvcConfigurer.java
+++ src/main/java/com/pet/demo/config/MyWebMvcConfigurer.java
@@ -1,8 +1,10 @@
 package com.pet.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
@@ -18,6 +20,14 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
 //        添加拦截路径
         registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/backstage/admin","/Apply/find","/PetTest/pet","/front/user","/manage","/info");
+    }
+    //启动时访问index，而不是index.html
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+    registry.addViewController("/").setViewName("forward:/index");
+    registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    WebMvcConfigurer.super.addViewControllers(registry);
     }
 
 }
